eca8cb9546a419c682ab33ec102045a02f09067c
NOVI <ppresnyakov@yandex.ru>
2023-12-12 11:05:24 +0300

    test

--- src/test/java/org/ibs/service/DbTest.java
+++ src/test/java/org/ibs/service/DbTest.java
@@ -7,9 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 
 
 @RunWith(SpringRunner.class)
